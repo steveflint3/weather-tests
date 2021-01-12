3e14e443ce3e6079af07fa24e6198eb3f2fefa43
steve <steve.flint@t1cg.com>
2021-01-12 15:48:57 -0500

    add in test

--- cypress/integration/weather_test.js
+++ cypress/integration/weather_test.js
@@ -1,13 +1,12 @@
 describe('Weather test', () => {
-  // it('tests the weather app', () => {
-  //   cy.visit('/');
-  //   cy.get('form').within(() => {
-  //     cy.get('input[name^="city"]').type('Baltimore');
-  //     cy.get('input[name^="country"]').type('United States');
-  //     cy.root().submit();
-  //   });
-  //   cy.get('p[class^="weather__key"]').contains('Location:').should('be.visible');
-  //   cy.get('span[class^="weather__value"]').contains('Baltimore, US').should('be.visible');
-  // });
-  console.log('fuck this shit');
+  it('tests the weather app', () => {
+    cy.visit('/');
+    // cy.get('form').within(() => {
+    //   cy.get('input[name^="city"]').type('Baltimore');
+    //   cy.get('input[name^="country"]').type('United States');
+    //   cy.root().submit();
+    // });
+    // cy.get('p[class^="weather__key"]').contains('Location:').should('be.visible');
+    // cy.get('span[class^="weather__value"]').contains('Baltimore, US').should('be.visible');
+  });
 });
